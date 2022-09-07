de2d32ad8c5f317301912257b41472cc817bce11
uuko <omkkqq@gmail.com>
2022-09-07 18:38:48 +0800

    "嘗試修改一版 主要修正web.xml "

--- src/main/java/com/test/jersey/DemoResource.java
+++ src/main/java/com/test/jersey/DemoResource.java
@@ -1,12 +1,9 @@
 package com.test.jersey;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 //
 // Pingable at:
--- src/main/java/com/test/jersey/HelloWorld.java
+++ src/main/java/com/test/jersey/HelloWorld.java
@@ -1,14 +1,24 @@
 package com.test.jersey;
 
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 
-@Path("/hello")
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
 public class HelloWorld {
+    @GET  @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "working...";
+    }
     //GET註解設置接受請求類型爲GET
+
     @GET
+    @Path("/hello")
     //Produces表明發送出去的數據類型爲text/plain
     // 與Produces對應的是@Consumes，表示接受的數據類型爲text/plain
     @Produces("text/plain")
